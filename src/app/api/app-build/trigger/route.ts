d580665a81c703f2946803175f6e704ff319e6c5
Molmurut46 <89045219234@mail.ru>
2025-10-16 19:45:29 +0000

    Update src/app/api/app-build/trigger/route.ts [skip ci]

--- src/app/api/app-build/trigger/route.ts
+++ src/app/api/app-build/trigger/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
 
+export const dynamic = "force-dynamic"
 export const runtime = "nodejs";
 
 const REQUIRED_VARS = [
