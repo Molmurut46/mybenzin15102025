394c2ce079dd7ac950359ab943aa9f4d52bd42b9
Molmurut46 <89045219234@mail.ru>
2025-10-16 18:25:37 +0000

    Update capacitor.config.ts [skip ci]

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.mybenzin.app',
   appName: 'Mybenzin',
   webDir: 'out',
-  server: {
-    // В production используем развернутый сайт
-    url: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
-    cleartext: true
-  },
   android: {
     backgroundColor: '#ffffff',
     allowMixedContent: true,
